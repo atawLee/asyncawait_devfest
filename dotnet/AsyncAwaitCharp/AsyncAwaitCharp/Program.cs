a702b3d81b47c7e7b6dd0dec75624af016d2ba0f
jonghoon <jonghoon.1738@gmail.com>
2024-11-17 21:52:48 +0900

    init

---
+++ dotnet/AsyncAwaitCharp/AsyncAwaitCharp/Program.cs
@@ -0,0 +1,19 @@
+﻿public partial class Program
+{
+    static async Task Main(string[] args)
+    {
+        await CallAsync();
+    }
+
+    private static async Task CallAsync()
+    {
+        string title = DateTime.Now.ToString();
+        string text = await GetFileContents();
+        Console.WriteLine(title + ": " + text);
+    }
+
+    private static async Task<string> GetFileContents()
+    {
+        return await new TaskFactory().StartNew(() => { return "test"; });
+    }
+}
